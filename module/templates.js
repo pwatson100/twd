cc2a941928d745b1fc69def2d5f5a35886b1058e
pwatson100 <paul@skynet2020.co.uk>
2023-05-17 19:36:17 +0100

    Css corrections

--- module/templates.js
+++ module/templates.js
@@ -8,14 +8,9 @@ export const preloadHandlebarsTemplates = async function () {
   const templatePaths = [
     // Actor Sheet Partials
     'systems/twd/templates/actor/tabs/actor-inventory.html',
-    'systems/twd/templates/actor/crt/tabs/actor-inventory.html',
     'systems/twd/templates/actor/tabs/vehicle-inventory.html',
-    'systems/twd/templates/actor/crt/tabs/vehicle-inventory.html',
-    'systems/twd/templates/actor/tabs/spacecraft-inventory.html',
     'systems/twd/templates/actor/tabs/actor-systems.html',
-    'systems/twd/templates/actor/crt/tabs/actor-systems.html',
     'systems/twd/templates/actor/tabs/critical-inj.html',
-    'systems/twd/templates/actor/crt/tabs/critical-inj.html',
   ];
 
   // Load the template parts
